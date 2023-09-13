383f7d42909a95ace68e38b23714de2a9cdb45ee
moohiit <pmohit645@gmail.com>
2023-09-13 14:24:47 +0530

    new files added

---
+++ BootCamp/Day4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    
+    // Question 1.
+    // int a= 15;
+    // int b=25;
+    // if((a<b) || (a=5)>15){
+    //     printf("%d",a);
+    // }else
+    //     printf("%d",b);
+
+    // Question 2.
+    // int x,y;
+    // x=15;y=20;
+    // if(x>15)
+    //     if(y>15){
+    //         printf("Y is %d",y);
+    //     }
+    //     else 
+    //         printf("Y is %d",y);
+
+    
+}
