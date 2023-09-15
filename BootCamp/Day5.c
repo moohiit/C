6669eecbec778c103b68cadcc86c0b4ac2c9cd5d
moohiit <pmohit645@gmail.com>
2023-09-15 14:02:10 +0530

    new files added

---
+++ BootCamp/Day5.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+int main()
+{
+
+    // Question 4.
+    //  int x=10;
+    //  int y=20;
+    //  if(x>y){
+    //      if(y>10){
+    //          printf("y is %d",y);
+    //      }
+    //      else
+    //          printf("x is %d",x);
+    //  }
+
+    // int j = 50;
+    // while (1)
+    // {
+    //     if (j < 10)
+    //         break;
+    //     j = j - 10;
+    // }
+    // printf("j is %d\n", j);
+
+    // int x=8;
+    // int y =(x>6?4:6);
+    // printf("Output: %d",y);
+
+    //     if(-5)
+    //     printf("a");
+    //     printf("b");
+    //     else
+    //     printf("c");
+    //     printf("d");
+
+    // if (-5)
+    // {
+    //     printf("a");
+    //     printf("b");
+    // }
+    // else
+    //     printf("c");
+    // printf("d");
+
+    // if (-5);
+    // printf("a");
+    // printf("b");
+    // else
+    // {
+    //     printf("c");
+    //     printf("d");
+    // }
+
+    // if (-5);
+    // printf("a");
+    // printf("b");
+    // else;
+    // {
+    //     printf("c");
+    //     printf("d");
+    // }
+
+    // if(5%5);{
+    //     printf("x");
+    //     printf("y");
+    // }
+    // printf("z");
+
+    if ("false") //condition true
+        ;
+    {
+        printf("x");
+        printf("y");
+    }
+    printf("z");
+}
+
---
+++ BootCamp/Day6.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+int main()
+{
+
+    // Question 1.
+    // if("false"){
+    //     printf("x");
+    //     printf("y");
+    // }
+    // printf("z");
+
+    // Question 2.
+    // int a=0,1;
+    // if (a)
+    // {
+    //     printf("x");
+    //     printf("y");
+    // }
+    // printf("z");
+
+    // if ('a')
+    // {
+    //     printf("x");
+    //     printf("y");
+    // }
+    // printf("z");
+
+    // if ()
+    // {
+    //     printf("x");
+    //     printf("y");
+    // }
+    // printf("z");
+
+    // if ("")
+    // {
+    //     printf("x");
+    //     printf("y");
+    // }
+    // printf("z");
+
+    // if ("mohit")
+    // {
+    //     printf("x");
+    //     printf("y");
+    // }
+    // printf("z");
+
+    // int a;
+    // a=(0,1);
+    // if (a)
+    // {
+    //     printf("x");
+    //     printf("y");
+    // }
+    // printf("z");
+
+    
+}
