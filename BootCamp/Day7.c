4be2178bc909a05be9f315668307c04951274c2f
moohiit <pmohit645@gmail.com>
2023-09-18 12:45:57 +0530

    new files added

---
+++ BootCamp/Day7.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main(){
+
+    // char arr[]= "Discovery Channel";
+    // printf("%s",arr);
+
+    // char str[] = {'g', 'l', 'o', 'b', 'e'};
+    // printf("%s",str);
+
+    // char str[] = {'g', 'l', 'o', 'b', 'a','l','\0'};
+    // printf("%s",str);
+
+    // char p[]="GODZILLA";
+    // int i=0;
+    // while (p[i]!='\0')
+    // {
+    //     printf("%c",*(p+i));
+    //     i++;
+    // }
+
+    // printf("%d",'\0');
+
+    // int arr[2][2]={1,2,3,4};
+    // printf("%u ,%u",arr+1 &arr+1);
+    
+    // int arr[10];
+    // printf("%d %d",arr[-2],arr[11]);
+
+    // int arr[10] ={1,2};
+    // printf("%d %d",arr[2],arr[11]);
+
+    // int arr[5]= {1,2,3,4,5};
+    // int p,q,r;
+    // p=++arr[1];
+    // q= arr[1]++;
+    // r=arr[p++];
+
+    // printf("%d %d %d",p,q,r);
+    
+    // int a[1]={100};
+    // printf("%d",0[a]);
+
+    // int a[1]={100};
+    // printf("%d",1[a]);
+
+    // int a[2]={100,200};
+    // printf("%d",1[a]);
+
+
+}
