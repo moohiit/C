8d37a302a8c489d4330b7653fed4e5cf8616c492
moohiit <pmohit645@gmail.com>
2023-09-20 13:02:55 +0530

    new files added

---
+++ BootCamp/Day9.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main(){
+
+    // struct emp{
+    //     char name[20];
+    //     int age;
+    //     float sal;
+    // };
+
+    // struct emp e ={"Tiger"};
+    // printf("%d %d %f",e.sal,e.age);
+
+
+    // union a
+    // {
+    //     int i;
+    //     char ch[2];
+    // };
+    // union a u;
+    // u.ch[0]=3;
+    // printf("%d",);
+
+
+    // union var
+    // {
+    //     int a,b;
+    // };
+    // union var v;
+    // v.a=10;
+    // v.b=20;
+    // printf("%d",v.a);
+
+
+    // struct value
+    // {
+    //     int bit1:1;
+    //     int bit3:4;
+    //     int bit4:4;
+    // } bit ={1,2,13};
+
+    // printf("%d %d %d",bit.bit1,bit.bit3,bit.bit4) ;  
+
+
+    // enum days {mon=-1,tue,wed=6,thu,fri,sat};
+    // printf("%d %d %d %d %d %d",mon,tue,wed,thu,fri,sat);
+
+
+    // enum status {pass,fail,absent};
+    // enum status stu1,stu2,stu3;
+    // stu1=pass;
+    // stu2=absent;
+    // stu3=fail;
+    // printf("%d %d %d",stu1,stu2,stu3);
+
+}
